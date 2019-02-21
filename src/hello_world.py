14e531d3eb7d34b4072e6104db88baf2e16fa4a0
Acsa Arcanjo <arcanjodagmar@gmail.com>
2019-02-21 10:04:59 -0300

    Incrementando o arquivo hello_world.py

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,5 +1,6 @@
 #arquivo de hello world para o tutorial de GIT
 
 print('OLÁ MUNDO!')
-print('este código está no meu primeiro repositório GIT!')
-
+print('Este código está no meu primeiro repositório GIT! E ele já está versionado no GITHUB')
+print('Se quiser conferir é só acessar')
+print('\nhttps://github.com/acsa-laa/first-repo-tutorial\n')
---
+++ src/hello_world.py~
@@ -0,0 +1,5 @@
+#arquivo de hello world para o tutorial de GIT
+
+print('OLÁ MUNDO!')
+print('este código está no meu primeiro repositório GIT!')
+
